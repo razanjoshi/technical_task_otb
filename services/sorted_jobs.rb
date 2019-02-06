c401f3f4b4e82044ddf8bb078398854203c4f732
Rajan Joshi <razan.joc@gmail.com>
2019-02-06 19:23:54 +0000

    Create a service object to trigger sorting.

---
+++ services/sorted_jobs.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# This class returns sorted list of jobs
+class SortedJobs
+  attr_reader :unsorted_jobs
+  def initialize(unsorted_jobs)
+    @unsorted_jobs = unsorted_jobs
+  end
+  
+  def call
+    jobs = JobsOrg.new(unsorted_jobs)
+  end
+end
